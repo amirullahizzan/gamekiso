e59e6c80b46e2dd71c02373ff57cdc931aae7333
Zan <38888951+amirullahizzan@users.noreply.github.com>
2023-07-19 10:32:35 +0900

    Delete ゲームプログラム基礎_課題07_2302gp0001_アミルライザン directory

--- ゲームプログラム基礎_課題07_2302gp0001_アミルライザン/program/Game.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define SCREEN_W	800
-#define SCREEN_H	450
-
-void GameInit();	//	‰Šú‰»ˆ—
-void GameUpdate();	//	XVˆ—
-void GameRender();	//	•`‰æˆ—
-void GameExit();	//	I—¹ˆ—
